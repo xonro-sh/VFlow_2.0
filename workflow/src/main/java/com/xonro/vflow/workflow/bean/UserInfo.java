8a3478c77dd1657abed939f7db4b64f308e09c85
louie-001 <louie@xonro.com>
2018-03-20 09:01:16 +0800

    用户信息

--- workflow/src/main/java/com/xonro/vflow/workflow/bean/UserInfo.java
+++ workflow/src/main/java/com/xonro/vflow/workflow/bean/UserInfo.java
@@ -7,6 +7,96 @@ import java.io.Serializable;
  * @date created in 2018-3-16 15:16
  */
 public class UserInfo implements Serializable{
+    private String tel;
+    private String workPlace;
+    private String mobile;
+    private boolean active;
+    private String position;
+    private String positionLevel;
+    private String hiredDate;
+    private String employeeType;
+    private String employeeState;
 
 
+    private String remark;
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getWorkPlace() {
+        return workPlace;
+    }
+
+    public void setWorkPlace(String workPlace) {
+        this.workPlace = workPlace;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getPositionLevel() {
+        return positionLevel;
+    }
+
+    public void setPositionLevel(String positionLevel) {
+        this.positionLevel = positionLevel;
+    }
+
+    public String getHiredDate() {
+        return hiredDate;
+    }
+
+    public void setHiredDate(String hiredDate) {
+        this.hiredDate = hiredDate;
+    }
+
+    public String getEmployeeType() {
+        return employeeType;
+    }
+
+    public void setEmployeeType(String employeeType) {
+        this.employeeType = employeeType;
+    }
+
+    public String getEmployeeState() {
+        return employeeState;
+    }
+
+    public void setEmployeeState(String employeeState) {
+        this.employeeState = employeeState;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
 }
