1ba5cc03174b5b362eb6c4e4afff3e213a556ac8
AlexWalker <1398730142@qq.com>
2018-04-24 17:55:49 +0800

    增加获取租户信息公共方法 以及登出方法

--- client/src/main/resources/static/js/util.js
+++ client/src/main/resources/static/js/util.js
@@ -13,3 +13,50 @@ function getUrlParameter(paramKey) {
         }
     }
 }
+
+function getTenantInfo(tenantId, layer) {
+    var info = {};
+    $.ajax({
+        url: "../../console/tenant_info",
+        type: "get",
+        dataType: "json",
+        data: {
+            tenantId: tenantId
+        },
+        async: false,
+        success: function (data) {
+            if ('ok' in data&&!data.ok){
+                layer.msg("获取租户信息失败,请联系相关人员");
+                setTimeout(function () {
+                    logout();
+                }, 2000)
+            } else {
+                info = data.data;
+            }
+        },
+        error : function (data) {
+            layer.msg("获取租户信息失败，错误信息"+data.msg, {icon: 2,time:3000});
+            setTimeout(function () {
+                logout();
+            }, 2000)
+        }
+    });
+    return info;
+}
+
+function logout() {
+    $.ajax({
+        url: "../../user/logout",
+        type: "get",
+        dataType: "json",
+        async: false,
+        success: function (data) {
+            window.location="../console/com.xonro.vflow_login.html";
+        },
+        error : function (data) {
+
+        }
+    });
+}
+
+
